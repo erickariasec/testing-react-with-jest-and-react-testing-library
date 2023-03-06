c3ab1d4a6fcd9c7137275228f3e030f77576f00c
erickariasec <erickarias1999@hotmail.com>
2023-03-06 12:59:46 -0500

    Using method for debugging roles: (logRoles)

--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, logRoles } from "@testing-library/react";
 import App from "./App";
 
 test("button has correct initial color", () => {
-  render(<App />);
+  const { container } = render(<App />);
+  logRoles(container);
 
   // find an element with a role of button and text of "Change to blue"
   const colorButton = screen.getByRole("button", { name: /change to blue/i });
